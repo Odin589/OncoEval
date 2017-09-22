5a8650f3c57af01ceae8b6c5b33ae9842e40fe25
lordleadhead <30911495+lordleadhead@users.noreply.github.com>
2017-09-21 20:12:10 -0500

    Error logging for py in PHP, Variable issue fixed

--- upload.php
+++ upload.php
@@ -9,6 +9,8 @@ if (isset($_POST['submit'])) {
     $fileError = $_FILES['file']['error'];
     $fileType = $_FILES['file']['type'];
     $fileDestination = '/Applications/MAMP/htdocs/CX3/uploads/';
+    $wholepath= $fileDestination.$fileName;
+
 
     //Getting file type extension
     $fileExplode = explode('.', $fileName);
@@ -17,6 +19,7 @@ if (isset($_POST['submit'])) {
     echo $_FILES['file']['name'];
     echo "fname: ".$fname."<br />";
     echo "file extention:".$fext."<br />";
+    echo $wholepath;
     //File types to allow
     $allowed = array('csv');
 
@@ -28,18 +31,22 @@ if (isset($_POST['submit'])) {
               //  if(!is_writable($fileDestination)){ echo "error in dir"; } //For debugging Ch
           //    else { //added for debugging
           if(is_uploaded_file($fileTmpName))
-          {echo "================================file uploaded=============================";}
+          {echo "================================file uploaded=============================\n";}
           if (move_uploaded_file($fileTmpName,"/Applications/MAMP/htdocs/CX3/uploads/".$fileName))
 
         //  if (move_uploaded_file($_FILES['file']['name'],"/Applications/MAMP/htdocs/CX3/uploads/"))
           {
           // $upload_message = $fileName.'has been uploaded';
                // header("Location: index.html?uploadsuccess");
-               echo "==========================================Your file is successfully moved=============================";
-              $mystring = system('python ./uploads/OncoEval_BCV2.py $fileName, $retval');
-              echo "==========================================Initiating Python script =============================";
+               echo "==========================================Your file is successfully moved=============================\n";
 
-             echo "================You fiile is being processedXPHPXXX ==========================";
+               echo "==========================================Initiating Python script =============================\n";
+              shell_exec ("python /Applications/MAMP/htdocs/CX3/uploads/OncoEval_BCV2.py $wholepath");
+
+
+            shell_exec ("python /Applications/MAMP/htdocs/CX3/uploads/OncoEval_BCV2.py $wholepath 2> ./python_error.log");
+
+             echo "================You fiile is being processedXPHPXXX ==========================\n";
               //echo "mystring = ".$mystring;
           //  }   //added for debugging
         } else {
